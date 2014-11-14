7b42ab2996c9b2128b81e9f4b156eb204f834097
manan <nxmanan@gmail.com>
2014-11-14 22:47:25 +0800

    update 1-16

--- beauty-of-programming/chap-1-game/1-16.cc
+++ beauty-of-programming/chap-1-game/1-16.cc
@@ -33,11 +33,61 @@ bool PointsGame(int n) {
       result[j] = result[n - 1];
 
       result[i] = '(' + expa + '+' + expb + ')';
+      number[i] = a + b;
+      if(PointsGame(n - 1))
+        return true;
+
+      result[i] = '(' + expa + '-' + expb + ')';
+      number[i] = a - b;
+      if(PointsGame(n - 1))
+        return true;
+
+      result[i] = '(' + expb + '-' + expa + ')';
+      number[i] = b - a;
+      if(PointsGame(n - 1))
+        return true;
+
+      result[i] = '(' + expa + '*' + expb + ')';
+      number[i] = a * b;
+      if(PointsGame(n - 1))
+        return true;
+
+      if (b != 0) {
+        result[i] = '(' + expa + '/' + expb + ')';
+        number[i] = a / b;
+        if(PointsGame(n - 1))
+          return true;
+      }
+      if (a != 0) {
+        result[i] = '(' + expa + '/' + expb + ')';
+        number[i] = b / a;
+        if(PointsGame(n - 1))
+          return true;
+      }
+
+      number[i] = a;
+      number[j] = b;
+      result[i] = expa;
+      result[j] = expb;
     }
   }
+  return false;
 }
 
 int main() {
-
+  int x;
+  for (int i = 0; i < CardsNumber; i++) {
+    char buffer[20];
+    cout << "the " << i << "th number:";
+    cin >> x;
+    number[i] = x;
+    sprintf(buffer, "%d", x);
+    result[i] = buffer;
+  }
+  if (PointsGame(CardsNumber)) {
+    cout << "Success." << endl;
+  } else {
+    cout << "Fail." << endl;
+  }
   return 0;
 }
