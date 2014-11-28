50f66110765be0c2282dfd3c8cab8a2b9f73a009
manan <nxmanan@gmail.com>
2014-11-28 17:31:14 +0800

    add 1.6

---
+++ cracking-the-coding-interview/chap01/1.6.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+
+void rotate(int **array, int n) {
+  if (array == NULL || n <= 1) {
+    return;
+  }
+
+  for ( int curSize = n; curSize > 1; curSize -= 2) {
+    for (int ind = 0; ind < curSize - 1; ind++) {
+      int curCircle = (n - curSize) / 2;
+      int tmp = array[curCircle][ind + curCircle];  //top
+      array[curCircle][ind + curCircle] = array[n - 1 - curCircle + ind][curCircle]; //left
+      array[n - 1 - curCircle + ind][curCircle] = array[n - 1 - curCircle][n - 1 - curCircle - ind]; //bottom;
+      array[n - 1 - curCircle][n - 1 - curCircle - ind] = array[curCircle + ind][n - 1 -curCircle];  //right
+      array[curCircle + ind][n - 1 -curCircle] = tmp;
+    }
+  }
+}
+
+void prtMat(int ** mat, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      std::cout << mat[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main() {
+  //  int array[2][2] = {{1, 2}, {4, 3}};
+  int **a = (int **) malloc(sizeof(int *) * 2);
+  int **tmp = a;
+  *tmp++ = (int *) malloc(sizeof(int) * 2);
+  *tmp = (int *) malloc(sizeof(int) * 2);
+  a[0][0] = 1;
+  a[0][1] = 2;
+  a[1][0] = 4;
+  a[1][1] = 3;
+
+  prtMat(a, 2);
+  rotate(a, 2);
+  prtMat(a, 2);
+}
