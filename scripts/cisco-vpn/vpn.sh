7124a7cfb91b86654331701b3a97b2a484dc7b37
manan <manan@fenbi.com>
2016-10-23 13:53:21 +0800

    MOD:README and scripts name

---
+++ scripts/cisco-vpn/vpn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]
+then
+    command=$1
+else
+    command='connect'
+fi
+
+if [ $command == 'stop' ]
+then
+    echo 'stop vpn'
+    /opt/cisco/anyconnect/bin/vpn disconnect
+    exit
+fi
+
+echo 'start vpn'
+username='[your ldap username]'
+passwd='[your ldap password]'`sh ./oath.sh [profile_name]`
+
+/opt/cisco/anyconnect/bin/vpn -s << EOF
+connect [vpn address]
+$username
+$passwd
+EOF
